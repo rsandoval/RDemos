9de6bed41ca21b084d341fe7aa0715002123325c
Rodrigo Sandoval <rodrigo@rsolver.com>
2020-03-06 13:30:18 -0300

    R:Demos: added the BankMessage demo

---
+++ RDemosNET/RDemosNET/Models/BankMessageCharacterizer.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace RDemosNET.Models
+{
+    public class BankMessageCharacterizer
+    {
+        const string _modelFolder = @"brain";
+        private static string _appPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        private static string _intentModelPath => Path.Combine(_modelFolder, "BankCommentTypeModel.zip");
+        private static string _subintentModelPath => Path.Combine(_modelFolder, "BankCommentSubtypeModel.zip");
+        private static string _emotionModelPath => Path.Combine(_modelFolder, "EmotionModel.zip");
+        private static string _intentionModelPath => Path.Combine(_modelFolder, "IntentionModel.zip");
+
+        private MLContext _mlContext;
+        private PredictionEngine<BankCustomerMessage, BankMessageTypePrediction> _messagePredEngine;
+        private PredictionEngine<BankCustomerMessage, BankMessageSubtypePrediction> _messageSubtypePredEngine;
+        private PredictionEngine<Comment, EmotionPrediction> _commentPredEngine;
+        private PredictionEngine<Comment, IntentionPrediction> _commentIntentionPredEngine;
+
+        public string Intent { get; set; }
+        public string Subintent { get; set; }
+        public string Emotion { get; set; }
+        public string Intention { get; set; }
+
+        public string RawContents { get; set; }
+
+        public BankMessageCharacterizer(string contents)
+        {
+            RawContents = contents;
+
+            _mlContext = new MLContext(seed: 0);
+            ITransformer intentLoadedModel = _mlContext.Model.Load(_intentModelPath, out var intentModelInputSchema);
+            _messagePredEngine = _mlContext.Model.CreatePredictionEngine<BankCustomerMessage, BankMessageTypePrediction>(intentLoadedModel);
+            ITransformer subintentLoadedModel = _mlContext.Model.Load(_subintentModelPath, out var subintentModelInputSchema);
+            _messageSubtypePredEngine = _mlContext.Model.CreatePredictionEngine<BankCustomerMessage, BankMessageSubtypePrediction>(subintentLoadedModel);
+
+            Intent = GetIntent();
+            Subintent = GetSubintent();
+
+            ITransformer emotionLoadedModel = _mlContext.Model.Load(_emotionModelPath, out var emotionModelInputSchema);
+            _commentPredEngine = _mlContext.Model.CreatePredictionEngine<Comment, EmotionPrediction>(emotionLoadedModel);
+            ITransformer intentionLoadedModel = _mlContext.Model.Load(_emotionModelPath, out var intentionModelInputSchema);
+            _commentIntentionPredEngine = _mlContext.Model.CreatePredictionEngine<Comment, IntentionPrediction>(intentionLoadedModel);
+
+            Emotion = GetEmotion();
+            Intention = GetIntention();
+        }
+
+        public string GetIntent()
+        {
+            BankCustomerMessage comment = new BankCustomerMessage() { Contents = RawContents };
+            var prediction = _messagePredEngine.Predict(comment);
+
+            string strIntent = prediction.TypeCode.ToString().ToLower();
+
+            return DescriptionsManager.GetInstance().GetDescription(strIntent);
+        }
+
+        public string GetSubintent()
+        {
+            BankCustomerMessage comment = new BankCustomerMessage() { Contents = RawContents };
+            var prediction = _messageSubtypePredEngine.Predict(comment);
+
+            string strSubintent = prediction.SubTypeCode.ToString().ToLower();
+
+            return strSubintent;
+        }
+
+        public string GetEmotion()
+        {
+            TextNormalizer textNormalizer = TextNormalizer.GetInstance();
+            Comment comment = new Comment() { Contents = textNormalizer.CleanString(RawContents) };
+            var prediction = _commentPredEngine.Predict(comment);
+
+            switch (prediction.Emotion)
+            {
+                case "0": return "alegría";
+                case "1": return "pena";
+                case "2": return "miedo";
+                case "3": return "disgusto";
+                case "4": return "sorpresa";
+                case "5": return "enojo";
+            }
+
+            return "-";
+        }
+
+        public string GetIntention()
+        {
+            //Comment comment = new Comment() { ID = "0", Contents = RawContents };
+            //var prediction = _commentIntentionPredEngine.Predict(comment);
+
+            //if (prediction.Intention.Contains("-"))
+            //    return "que va a ocurrir";
+            //else return "que ya ocurrió";
+            string strContents = TextNormalizer.GetInstance().CleanString(RawContents);
+            if (strContents.Contains("ar ") || strContents.Contains("er ") || strContents.Contains("rias ") || strContents.Contains("ras "))
+                return "que pide que ocurra";
+            else return "que ya ocurrió";
+        }
+        public string GetDescription()
+        {
+            string intent = Intent.ToLower();
+            string subintent = Subintent;
+            string emotion = Emotion;
+            string intention = Intention;
+
+            Random randomGen = new Random(DateTime.Now.Millisecond);
+            string[] startingPhrases = {
+                "Ojo, que el cliente está presentando un",
+                "Este es claramente un",
+                "OK. En este caso el cliente está pidiendo un",
+                "El cliente necesita un",
+                "La petición del cliente es de un",
+                "Aquí se está pidiendo un" };
+            string[] connectingPhrases = {
+                " de ",
+                " y ojo con la "};
+
+            string description = startingPhrases[randomGen.Next(startingPhrases.Length - 1) + 1]; // index = 0 es reclamos
+
+            if (intent.StartsWith("consulta") || intent.StartsWith("queja"))
+            {
+                description += "a <b>" + intent + "</b>, subcódigo <b>" + subintent + "</b>, " + intention;
+                if (randomGen.Next(2) == 1 && (emotion != "enojo" && !RawContents.ToLower().Contains("gracia") && !RawContents.ToLower().Contains("cordiale")))
+                    description += " y lo pide con " + emotion + ".";
+            }
+            else if (intent.Contains("reclamo") || intent.Contains("queja"))
+            {
+                description = startingPhrases[randomGen.Next(2)] + " <b>" + intent + "</b>";
+                if (!String.IsNullOrEmpty(subintent))
+                    description += " respecto a un código <b>" + subintent + "</b>, " + intention;
+                description += ". Nótese que se siente " + emotion + " en el mensaje.";
+            }
+            else if (!String.IsNullOrEmpty(subintent))
+            {
+                description += " <b>" + intent + "</b> de un" + (subintent.EndsWith("a") ? "a " : " ") + "<b>" + subintent + "</b>";
+            }
+            else
+            {
+                description += " <b>" + intent + "</b> de algo indeterminado, " + intention;
+            }
+
+            return description;
+        }
+
+        public string GetSimpleDescription()
+        {
+            string intent = Intent;
+            string subintent = Subintent;
+            string emotion = Emotion;
+            string intention = Intention;
+
+            string description = "Solicitud: <b>" + intent + "</b><br />"
+                + "Subcódigo: <b>" + subintent + "</b><br />"
+                + "Emoción: <b>" + emotion + "</b><br />"
+                + "Intención: <b>" + intention + "</b>"; 
+            return description;
+        }
+    }
+
+
+    public class DescriptionsManager
+    {
+        const string _dataFolder = @"data";
+        private static string _appPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        private static string _intentDescriptions => Path.Combine(_dataFolder, "BankIntentDescription.txt");
+
+        private static DescriptionsManager _single = null;
+        List<Tuple<int, string>> _codesAndDescriptions = new List<Tuple<int, string>>();
+
+        private DescriptionsManager()
+        {
+            _codesAndDescriptions = LoadDataFile(_intentDescriptions);
+        }
+
+        public static DescriptionsManager GetInstance()
+        {
+            if (_single == null) _single = new DescriptionsManager();
+            return _single;
+        }
+
+        private List<Tuple<int, string>> LoadDataFile(string filename)
+        {
+            List<Tuple<int, string>> pairs = new List<Tuple<int, string>>();
+            try
+            {
+                System.IO.StreamReader reader = new System.IO.StreamReader(filename);
+                string dataLine = reader.ReadLine();
+                while (!string.IsNullOrEmpty(dataLine))
+                {
+                    char[] separator = { ';' };
+                    string[] tokens = dataLine.Split(separator);
+                    int code = 0; int.TryParse(tokens[0], out code);
+                    Tuple<int, string> pair = new Tuple<int, string>(code, tokens[1]);
+                    pairs.Add(pair);
+                    dataLine = reader.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+            }
+
+            return pairs;
+        }
+
+        public string GetDescription(int code)
+        {
+            foreach (Tuple<int, string> pair in _codesAndDescriptions)
+                if (code == pair.Item1)
+                    return pair.Item2;
+
+            return "-";
+        }
+
+        public string GetDescription(string strCode)
+        {
+
+            int code = 0; int.TryParse(strCode, out code);
+            if (code >= 0)
+                return GetDescription(code);
+
+            return "-";
+        }
+    }
+
+    public class BankCustomerMessage
+    {
+        private string _cleanContents = "";
+        // public string OriginalContents = "";
+
+        [LoadColumn(0)]
+        public string Contents { get { return _cleanContents; } set { _cleanContents = CleanContent(value); } }
+        [LoadColumn(1)]
+        public string TypeCode { get; set; }
+        [LoadColumn(2)]
+        public string SubTypeCode { get; set; }
+
+        public string CleanContent(string contents)
+        {
+            TextNormalizer normalizer = TextNormalizer.GetInstance();
+            contents = normalizer.CleanString(contents);
+            return contents;
+        }
+    }
+
+    public class BankMessageTypePrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string TypeCode;
+    }
+
+    public class BankMessageSubtypePrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string SubTypeCode;
+    }
+}
---
+++ RDemosNET/RDemosNET/Pages/RAlizeBankMsg.cshtml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using RDemosNET.Models;
+
+namespace RDemosNET
+{
+    public class RAlizeBankMsgModel : PageModel
+    {
+        [BindProperty]
+        public string MessageContents { get; set; }
+
+        public string MessageDescription { get; set; }
+
+        public string MessageShortDescription { get; set; }
+
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPostProcessDocument(string txtContents)
+        {
+            if (String.IsNullOrEmpty(txtContents))
+            {
+                MessageDescription = "No hay mensaje a procesar.";
+                return;
+            }
+            MessageCharacterizer characterizer = new MessageCharacterizer(txtContents);
+            MessageContents = txtContents;
+            MessageDescription = characterizer.GetDescription();
+        }
+        public void OnPostRandomSample()
+        {
+            Random randomNum = new Random(DateTime.Now.Millisecond);
+            string[] samples = {
+                "Hola Adrián, no puedo hacer transferencias, se queda pegado en la llamada de la clave dinámica.",
+                "Estimada María Soledad. Ante todo desearte un estupendo año¡¡¡¡...  Al revisar la cuenta corriente figura un cargo por seis mil y algo pesos. De acuerdo a lo contratado no habría recargo y yo no he utilizado aún ninguno de mis productos. Me puedes contar a que se debe?...  Agradecería me indicaras también, si sabes algo del re financiamiento de mi propiedad. Estoy a la espera de esa respuesta para cerrar mis otros productos y transferir todo a esta cuenta corriente.  Agradezco tus gestiones,  Sa",
+                "Estimados señores: Quisiera comentarles que en tres ocasiones dentro de este año he intentado contactar a mi ejecutiva por esta vía, no habiendo tenido respuesta de parte ",
+                "El 1 de marzo compre el SOAP con mi tarjeta de credito mastercard y a la fecha aun no recibo los creditos cabify ofrecidos por la compra.",
+                "estimada Gabriela podrias trnsferir de mi cuenta corriente 100.000 pesos a mi deposito a plazo atenta a su respuesta",
+                "Hola  quisiera dar de baja un seguro de fraude que tengo contratado.   desde ya gracias,  saludos. ",
+                "Katia,  Me podrias por favor indicar cuales son als condiciones de prepago del credito de consumo con tasa preferencial.  Quiero solicitarlo pero necesito saber cuales son las condiciones de prepago.  Muchas gracias.  Carolina",
+                "Gina  Necesito que me permitan usar tarjeta de crédito en Asia, desde el día 12 de enero al día 5 de febrero 2018; además, necesito que me informen monto de comisión por sacar plata y por comprar con la tarjeta.  Gracias,  Pamela ",
+                "Hola, requiero las cartolas históricas de mi cuenta corriente desde enero del 2013 a Marzo del 2016. No me aparecen en el portal las antiguas. Solo desde abril 2016.  Muchas ",
+                "Estimada Marcela Agradeceré me ayude con este cargo ya que está duplicado, solo tiene que haber 1: 24-Ago-2018  PORTEZUELO VITACURA SANTIAGO CC 01-03  ",
+                "hola Carolina, por favor pagar credito de consumo.  muy felices fiestas.  mil gracias",
+                "Hola Claudia. Por favor te pido me puedan contactar para revisar que opciones me pueden ofrecer para un crédito de consumo. Muchas Gracias.",
+                "Nicolas porfavor da termino al cobro de los Seguros de Hogar que tengo solo deseo continuar con los seguros de fraude agradecida saluda atte  Patricia",
+                "Al revisar el día de hoy mi cuenta me percato que me han cobrado de forma automática el pago mínimo de la linea de crédito cargándomelo en la corriente, siendo que este fue pagado el 28 de febrero. Poseo el comprobante de pago realizado el 28 de febrero, y también el comprobante del cobro que me han realizado el día de hoy. "
+            };
+            string strComment = samples[randomNum.Next(samples.Length)];
+            MessageContents = strComment;
+            BankMessageCharacterizer characterizer = new BankMessageCharacterizer(strComment);
+            MessageDescription = characterizer.GetDescription();
+            MessageShortDescription = characterizer.GetSimpleDescription();
+        }
+
+        public string GetDescriptionAsHTML(BankMessageCharacterizer characterizer)
+        {
+            string descAsHTML = characterizer.GetDescription();
+
+
+            return descAsHTML;
+        }
+
+    }
+}
