727428c8e0445863f3f0b46919783ed507fa079c
Rodrigo Sandoval <rodrigo@rsolver.com>
2022-07-13 17:09:17 -0400

    R:DocsCV - Adding Log

---
+++ RDemosNET/RDemosNET/Migrations/20210121192411_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RDemosNET.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DemoLog",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApplicationID = table.Column<int>(nullable: false),
+                    LogDate = table.Column<DateTime>(nullable: false),
+                    IPAddress = table.Column<string>(nullable: true),
+                    LogDetails = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DemoLog", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DemoLog");
+        }
+    }
+}
---
+++ RDemosNET/RDemosNET/Models/DemoLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RDemosNET.Models
+{
+    public class DemoLog
+    {
+        public int ID { get; set; }
+        public int ApplicationID { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime LogDate { get; set; }
+        public string IPAddress { get; set; }
+        public string LogDetails { get; set; }
+    }
+}
